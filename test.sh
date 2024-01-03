a2a979ee8965ee63b8e2a5615f1a94f5848c59ef
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-03 01:46:19 -0800

    Add new API call for retrieving all comments

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@ curl http://192.168.0.91:8181/addcom/Herman%20Munster/herman@monster.com/this%20
 
 
 curl http://192.168.0.91:8181/addcom/casper/casper@monster.com/this%20is%20another%20commit%20from%20casper;
+
+curl http://192.168.0.91:8181//allcom
