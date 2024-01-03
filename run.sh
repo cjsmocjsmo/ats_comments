d070c2df0d6c37929773477c9f92ed33728efa13
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-03 04:02:14 -0800

    Add atscomments.service and update run.sh script

--- run.sh
+++ run.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-cd /usr/share/ats_comments;
-rm -rf uploads;
+if [ -d "/usr/share/ats_comments/uploads" ]; then
+    rm -rf "/usr/share/ats_comments/uploads";
+fi 
 echo "Removed uploads dir";
-cd /usr/share/ats_comments/ats_comments;
-rm -rf db;
+
+if [ -d "/usr/share/ats_comments/ats_comments/db"]; then
+    rm -rf "/usr/share/ats_comments/ats_comments/db";
+fi 
 echo "Removed db dir";
 
 git pull;
-RUST_LOG=info cargo run --release
+
+RUST_LOG=info cargo build --release;
+# cargo build --release;
+
+# ATSCOMMENTS = "/usr/share/ats_comments/ats_comments/target/release/ats_comments";
+
+# if [ ! -f /usr/bin/ats_comments ]; then
+#     sudo cp -pvr $ATSCOMMENTS /usr/bin/;
+#     sudo chown root:root /usr/bin/ats_comments;
+#     sudo chmod +x /usr/bin/ats_comments;
+# fi
+
+# if [ -f /usr/bin/ats_comments ]; then
+#     sudo rm /usr/bin/ats_comments;
+#     sudo cp -pvr $ATSCOMMENTS /usr/bin/;
+#     sudo chown root:root /usr/bin/ats_comments;
+#     sudo chmod +x /usr/bin/ats_comments;
+# fi
+
+# SYSD="/etc/systemd/system/";
+# SERVFILE="/usr/share/ats_comments/ats_comments/ats_comments.service";
+# if [ ! -f $SYSD"/ats_comments.service" ]; then
+#     sudo cp -pvr $SERVFILE $SYSD;
+# fi 
+
+# sudo systemctl start ats_comments.service;
+# sudo systemctl enable ats_comments.service
