9716c36ebbacdca40af23ae5939a0febb2e3ec66
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-05 00:49:24 -0800

    Fix sendmail directory check and update build process

--- run.sh
+++ run.sh
@@ -5,7 +5,13 @@ if [ ! -d "/usr/share/sendmail" ]; then
     exit 1;
 fi
 
-if [ -d "/usr/share/sendmail/sendmail" ]; then
+if [ ! -d "/usr/share/sendmail/sendmail" ]; then
+    cd "/usr/share/sendmail";
+    git clone "https://github.com/cjsmocjsmo/sendmail.git";
+    cd "/usr/share/ats_comments/ats_comments";
+    go build;
+    cd "/usr/share/ats_comments/ats_comments";
+else if [ -d "/usr/share/sendmail/sendmail" ]; then
     cd /usr/share/sendmail/sendmail;
     git pull;
     go build;
