f676f2eefebc204c2fc5959862713f34fa0d8193
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-05 01:55:17 -0800

    Update test.sh and add test_sendmail.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-# curl http://192.168.0.91:8181/addcom/Herman%20Munster/herman@monster.com/this%20is%20the%20first%20comment%20lets%20see%20if%20it%20works;
+#!/bin/bash
 
-# curl http://192.168.0.91:8181/addcom/casper/casper@monster.com/this%20is%20another%20commit%20from%20casper;
+curl http://192.168.0.91:8181/addcom/Herman%20Munster/herman@monster.com/this%20is%20the%20first%20comment%20lets%20see%20if%20it%20works;
+
+curl http://192.168.0.91:8181/addcom/casper/casper@monster.com/this%20is%20another%20commit%20from%20casper;
 
 curl http://192.168.0.91:8181/allcom;
 
@@ -11,14 +13,14 @@ curl http://192.168.0.91:8181/allesti;
 
 
 
-# SENDMAIL="/media/charliepi/ATree/AlphaTree/sendmail/sendmail"
-# $SENDMAIL \
-#     -etype "esti" \
-#     -name "Herman Munster" \
-#     -address "555 Mockingbird Lane" \
-#     -city "Mockingbird Heights" \
-#     -phone "555-555-5555" \
-#     -email "herman@monster.com" \
-#     -comment "This is the first comment lets see if it works" \
-#     -intake "01-02-2023";
+SENDMAIL="/usr/share/sendmail/sendmail/sendmail"
+$SENDMAIL \
+    -etype "esti" \
+    -name "Herman Munster" \
+    -address "555 Mockingbird Lane" \
+    -city "Mockingbird Heights" \
+    -phone "555-555-5555" \
+    -email "herman@monster.com" \
+    -comment "This is the first comment lets see if it works" \
+    -intake "01-02-2023";
     
