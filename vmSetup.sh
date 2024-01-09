fab6d291c64f6494dacb7aa9cd4f38f5448cf4c0
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-09 01:46:11 -0800

    update

--- vmSetup.sh
+++ vmSetup.sh
@@ -5,7 +5,7 @@ sudo apt -y dist-upgrade;
 sudo apt -y autoremove;
 sudo apt -y autoclean;
 sudo apt -y install build-essential sqlite3 libsqlite3-dev openssl libssl-dev pkg-config git curl;
-sudo snap install --classic cerbot;
+sudo snap install --classic certbot;
 sudo ln -s /snap/bin/certbot /usr/bin/certbot;
 sudo certbot certonly --standalone;
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
